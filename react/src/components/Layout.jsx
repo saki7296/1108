2f1a39921ec0e1850a076c06ecc7ba2ac36fafe7
saki7296 <jing10017296@gmail.com>
2024-11-08 14:04:50 +0800

    Add files via upload

---
+++ react/src/components/Layout.jsx
@@ -0,0 +1,24 @@
+import {Link, Outlet} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/get">Get All Users</Link>
+                </li>
+                <li>
+                    <Link to="/post">Post User</Link>
+                </li>                
+            </ul>
+        </nav>
+        <Outlet/>
+        </>
+    );
+};
+
+export default Layout;
